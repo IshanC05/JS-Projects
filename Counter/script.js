76ba35d4497297495c7f5ca131bf4d77403c0f28
Ishan <66194998+IshanC05@users.noreply.github.com>
2023-07-07 18:55:30 +0530

    Add files via upload

---
+++ Counter/script.js
@@ -0,0 +1,31 @@
+var btn = document.querySelector('.start-button');
+
+var count = 0;
+
+btn.addEventListener('click', startCounter);
+
+var current = document.querySelector('.current');
+var next = document.querySelector('.next');
+var maxVal = document.getElementById('cnt-input').value;
+
+current.innerHTML = count;
+
+function startCounter() {
+    current.innerHTML = count;
+    var id = setInterval(animate, 1000);
+}
+
+function animate() {
+    if (maxVal == count) {
+        clearInterval(id);
+        count = 0;
+        current.innerHTML = count;
+        return;
+    }
+    next.innerHTML = ++count;
+    next.classList.add('animate');
+    setTimeout(function () {
+        current.innerHTML = count;
+        next.classList.remove('animate');
+    }, 500);
+}
